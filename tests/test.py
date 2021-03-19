bcbfc61baa06fccdd14eb247cf9e5087b1c369ad
Max Zhenzhera <megashurik@urk.net>
2021-03-19 19:34:51 +0200

    added tests package (tested while developing)

---
+++ tests/test.py
@@ -0,0 +1,3 @@
+"""
+Here might be tests
+"""
---
+++ tests/test_weather_api.py
@@ -0,0 +1,9 @@
+"""
+Test weather api implementation for discord bot
+"""
+
+import asyncio
+
+import aiohttp
+
+from discord_weather_bot import weather_api
