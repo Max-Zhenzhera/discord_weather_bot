a5798985ba5b0ceb8a0951e3eec9363714fcb709
Max Zhenzhera <megashurik@urk.net>
2021-04-08 15:39:07 +0300

    'temp'

--- discord_weather_bot/utils/logging_/logging_.py
+++ discord_weather_bot/utils/logging_/logging_.py
@@ -15,7 +15,7 @@ from typing import (
 import yaml
 
 from ...settings import CORE_DIR
-
+from ... import settings
 
 def setup_logging(config_path: pathlib.Path, default_level: Union[int, str] = logging.INFO) -> None:
     """
@@ -39,7 +39,16 @@ def setup_logging(config_path: pathlib.Path, default_level: Union[int, str] = lo
                     if 'file' in handler_name:
                         log_path_from_main_package = config['handlers'][handler_name]['filename']
                         log_path = (CORE_DIR / log_path_from_main_package).resolve()
+
+                        print(settings.PROJECT_DIR)
+                        print(settings.CORE_DIR)
+                        print(settings.LOGGING_CONFIG_PATH)
+                        print(log_path)
+
                         log_dir_path = log_path.parent
+
+                        print(log_path)
+                        
                         log_dir_path.mkdir(parents=True, exist_ok=True)
 
                 logging.config.dictConfig(config)
