6a10fc0bcd4186d467837a8a73922a205cc8fc96
Max Zhenzhera <megashurik@urk.net>
2021-04-25 09:26:46 +0300

    added package to global path

--- discord_weather_bot/__main__.py
+++ discord_weather_bot/__main__.py
@@ -1,10 +1,18 @@
 """
-Entry point
+Entry point.
 """
 
-from discord_weather_bot import bot
+# add package to global path -------------------------------------------------------------------------------------------
+import sys
+import pathlib
+
+
+sys.path.append(pathlib.Path(__file__).parent.parent.__str__())
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
+    from discord_weather_bot import bot
+
     discord_weather_bot = bot.DiscordWeatherBot()
     discord_weather_bot.run()
